9c8d1a15831905e4bd09b184d4653b1e8faed4ce
King Georges <opondigeorge@gmail.com>
2023-06-21 06:27:00 +0300

    streamlit

--- Heart_Disease_prediction_app.py
+++ Heart_Disease_prediction_app.py
@@ -7,3 +7,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('Heart_Disease_Prediction.csv')
+
+st.title('Heart Disease Prediction App')
+st.write("""
+# Heart Disease Prediction App
+
+This app predicts the likelihood of having Heart Disease
+
+Data obtained from the University of California Irvine data repository and is used to predict heart disease
+ https://data.world/informatics-edu/heart-disease-prediction/workspace/file?filename=+Heart_Disease_Prediction.csv
+""")

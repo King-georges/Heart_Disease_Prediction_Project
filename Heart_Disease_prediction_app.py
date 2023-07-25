aed2ee9845413fad54a2f91a49cc5d8dbb30ab8f
King Georges <opondigeorge@gmail.com>
2023-07-25 20:33:53 +0300

    s-S

--- Heart_Disease_prediction_app.py
+++ Heart_Disease_prediction_app.py
@@ -118,7 +118,7 @@ def local_css(file_name):
      with open(file_name) as f:
           st.markdown (f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("Style/style.css")
 
 with st.container():
      st.write("---")
